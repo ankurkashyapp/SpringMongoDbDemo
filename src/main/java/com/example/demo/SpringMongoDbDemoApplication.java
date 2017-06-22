25684261ee84f3cb4041280b1ed8a1329483c0d3
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2017-06-22 17:28:31 +0530

    First Commit

---
+++ src/main/java/com/example/demo/SpringMongoDbDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbDemoApplication.class, args);
+	}
+}
