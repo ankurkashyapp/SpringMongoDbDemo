dc59f74dbaa1bb1040b5d9b65512e71945abe278
Ankur Kashyap <shiv.ankurkashyap@gmail.com>
2017-06-23 12:16:09 +0530

    Modified some file

--- src/main/java/com/example/demo/SimpleRedisRunner.java
+++ src/main/java/com/example/demo/SimpleRedisRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,8 +13,12 @@ public class SimpleRedisRunner implements CommandLineRunner {
 	@Autowired
 	StringRedisTemplate redistemplate;
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public void run(String... arg0) throws Exception {
+		System.out.println(jdbcTemplate);
 		ValueOperations<String, String> operations = redistemplate.opsForValue();
 		operations.set("foo", "bar");
 		System.out.println(operations.get("foo"));
